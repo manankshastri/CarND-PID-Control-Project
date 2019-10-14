bde022a38d75e5e69d76e43cb11c7fe4ac91187e
manankshastri <manank.shastri@gmail.com>
2019-10-14 04:41:28 -0400

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,7 +12,9 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
-
+   Kp = Kp_;
+   Ki = Ki_;
+   Kd = Kd_;
 }
 
 void PID::UpdateError(double cte) {
